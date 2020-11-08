0ad4ba00f7a2ac71203e14dbab6a5de5fc963b3a
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-11-08 18:37:50 +0100

    .

---
+++ Blauw3/Assets/Scripts/DungeonGeneration/DungeonGeneration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGeneration : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+/* instantiate room
+ * check if "doors" already have rooms
+ * randomly spawn room at "door" for every room
+ * repeat process for spawned room
+ * when max room is reached, closes al non connected "doors"
+ */
