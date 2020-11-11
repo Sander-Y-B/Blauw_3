e96afa4f380c710433c57133fe7f7af855a0f409
Sander Y B <sanderybrinkman@live.nl>
2020-11-11 14:27:47 +0100

    WIP added gun gen v1
    added a demo vers of the gun spawner
    (1) to delete current gun and spawn a new one
    added 3 new tages

---
+++ Blauw3/Assets/Sander/Scripts/GunManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunManager : MonoBehaviour
+{
+    public GameObject[] gunBodies;
+    public GameObject[] gunLoaders;
+    public GameObject[] gunBarrels;
+
+    GameObject loaderPoint;
+    GameObject barrelPoint;
+    GameObject shotPoint;
+    GameObject currentGunBody;
+
+
+    private void Start()
+    {
+        SpawnNewGun();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnNewGun();
+        }
+    }
+
+    void SpawnNewGun()
+    {
+        if (currentGunBody != null)
+        {
+            Destroy(currentGunBody);
+        }
+        else
+        {
+            InstBody();
+            InstLoader();
+            InstBarrel();
+        }
+
+    }
+
+    void InstBody()
+    {
+        currentGunBody = Instantiate(gunBodies[Random.Range(0, gunBodies.Length)], gameObject.transform, false);
+        loaderPoint = GameObject.FindGameObjectWithTag("GunLoaderPoint");
+    }
+
+    void InstLoader()
+    {
+        Instantiate(gunLoaders[Random.Range(0, gunLoaders.Length)], loaderPoint.transform, false);
+        barrelPoint = GameObject.FindWithTag("GunBarrelPoint");
+    }
+
+    void InstBarrel()
+    {
+        Instantiate(gunBarrels[Random.Range(0, gunBarrels.Length)], barrelPoint.transform, false);
+        shotPoint = GameObject.FindWithTag("ShotPoint");
+    }
+
+}
