36b0e8bac6b73f22039e1c5c2ba52264fc2778d4
EdgarSabel <60340099+EdgarSabel@users.noreply.github.com>
2020-11-18 19:22:56 +0100

    main menu push
    sander meta file?

---
+++ Blauw3/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject main, options;
+    public void OpenOptions()
+    {
+        main.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void OpenMain()
+    {
+        options.SetActive(false);
+        main.SetActive(true);
+    }
+    public void LoadGame(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void QuitGame()
+    {
+        print("Quit");
+        Application.Quit();
+    }
+}
