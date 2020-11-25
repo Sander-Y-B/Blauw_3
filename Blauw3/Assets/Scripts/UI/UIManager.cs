6296730bbfbdbf28a09f115129f60b088f06d488
EdgarSabel <60340099+EdgarSabel@users.noreply.github.com>
2020-11-25 11:08:41 +0100

    wip, G drukken geeft error pas op :P

--- Blauw3/Assets/Scripts/UI/UIManager.cs
+++ Blauw3/Assets/Scripts/UI/UIManager.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class UIManager : MonoBehaviour
 {
     public static bool isPaused;
     private float newPercent;
-    public GameObject player, pause, options, mainPause;
+    public GameObject player, pause, options, mainPause, gunStat;
     public Slider mainSlider, musicSlider, sfxSlider, healthSlider;
+    public GameObject damageText, fireRateText, clipSizeText, spreadText, scopedSpreadText, reloadSpeedText, recoilText;
 
     private void Update()
     {
+        if (Input.GetButtonDown("GunStat"))
+        {
+            ToggleGunStat();
+            UpdateStats(1f,1f,1f,1f,1f,1f,1f);
+        }
+
         if (Input.GetButtonDown("Cancel"))
         {
             TogglePause();
@@ -22,6 +30,22 @@ public class UIManager : MonoBehaviour
     {
         newPercent = newHealth/ maxHealth * 100;
         healthSlider.value = newPercent;
+    }
+
+    public void UpdateStats(float damage, float fireRate, float clipSize, float spread, float scopedSpread, float reloadSpeed, float recoil)
+    {
+        damageText.GetComponent<TextMeshPro>().text = damage.ToString();
+        spreadText.GetComponent<TextMeshPro>().text = spread.ToString();
+        recoilText.GetComponent<TextMeshPro>().text = recoil.ToString();
+        fireRateText.GetComponent<TextMeshPro>().text = fireRate.ToString();
+        clipSizeText.GetComponent<TextMeshPro>().text = clipSize.ToString();
+        reloadSpeedText.GetComponent<TextMeshPro>().text = reloadSpeed.ToString();
+        scopedSpreadText.GetComponent<TextMeshPro>().text = scopedSpread.ToString();
+    }
+
+    public void ToggleGunStat()
+    {
+        gunStat.GetComponent<Animator>().SetBool("GunStat", !gunStat.GetComponent<Animator>().GetBool("GunStat"));
     }
     public void TogglePause()
     {
