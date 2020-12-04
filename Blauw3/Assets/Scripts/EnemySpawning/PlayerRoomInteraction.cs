6b6e6641c3df6c0224c816e130dbbad8fe369a10
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-12-04 10:51:31 +0100

    Room spawn working (doors closing and opening not included)

---
+++ Blauw3/Assets/Scripts/EnemySpawning/PlayerRoomInteraction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRoomInteraction : MonoBehaviour
+{
+    public string roomTagName;
+
+    public void OnTriggerEnter(Collider o)
+    {
+        if(o.gameObject.tag == roomTagName)
+        {
+            o.gameObject.GetComponent<SpawnEnemies>().RoomStart();
+        }
+    }
+}
--- Blauw3/Assets/Scripts/EnemySpawning/SpawnEnemies.cs
+++ Blauw3/Assets/Scripts/EnemySpawning/SpawnEnemies.cs
@@ -4,50 +4,57 @@ using UnityEngine;
 
 public class SpawnEnemies : MonoBehaviour
 {
+    public int totalEnemies;
+    private int currentEnemies;
     public int maxEnemiesAllowed;
     public float minTimeBetweenSpawns;
+    public int[] spawnChanceEnemy;
+    private int enemiesSpawnIndicator;
     public GameObject[] spawnPoints;
     public List<GameObject> enemiesToSpawn;
     public List<GameObject> enemiesAlive;
     private GameObject justSpawnedEnemy;
-    private int enemiesToSpawnIndicator;
     private float timeSinceLastSpawn;
     private bool timerBool;
     private bool roomCleared;
 
     void Start()
     {
-        RoomStart();
+        //RoomStart(); //for testing purposes only
     }
 
     void Update()
     {
-        if(timeSinceLastSpawn >= minTimeBetweenSpawns && enemiesToSpawn.Count > 0 && enemiesAlive.Count < maxEnemiesAllowed)
+        if (roomCleared == false)
         {
-            enemiesToSpawnIndicator = Random.Range(0, enemiesToSpawn.Count);
-            justSpawnedEnemy = Instantiate(enemiesToSpawn[enemiesToSpawnIndicator], spawnPoints[Random.Range(0 ,spawnPoints.Length)].transform.position, Quaternion.identity);
-            enemiesAlive.Add(justSpawnedEnemy);
-            enemiesToSpawn.Remove(justSpawnedEnemy);
-            timeSinceLastSpawn = 0;
-        }
+            if(timeSinceLastSpawn >= minTimeBetweenSpawns && currentEnemies < totalEnemies && enemiesAlive.Count < maxEnemiesAllowed)//spawns more enenmies, but not over a certain amount and there is a minimum wait time
+            {
+                indicateSpawnEnemy();
+                justSpawnedEnemy = Instantiate(enemiesToSpawn[enemiesSpawnIndicator], spawnPoints[Random.Range(0 ,spawnPoints.Length)].transform.position, Quaternion.identity);
+                enemiesAlive.Add(justSpawnedEnemy);
+                enemiesToSpawn.Remove(justSpawnedEnemy);
+                currentEnemies++;
+                timeSinceLastSpawn = 0;
+            }
 
-        if(timerBool == true)
-        {
-            timeSinceLastSpawn += Time.deltaTime;
-        }
+            if(timerBool == true)
+            {
+                timeSinceLastSpawn += Time.deltaTime;
+            }
 
-        if(enemiesToSpawn.Count == 0 && enemiesAlive.Count == 0)
-        {
-            RoomCleared();
+            if(totalEnemies == currentEnemies && enemiesAlive.Count == 0)
+            {
+                RoomCleared();
+            }
         }
     }
 
-    public void RoomStart()
+    public void RoomStart() //called when player enters the room for the first time (though an on trigger on the player)
     {
         foreach(GameObject spawnPoint in spawnPoints)
         {
-            enemiesToSpawnIndicator = Random.Range(0, enemiesToSpawn.Count);
-            justSpawnedEnemy = Instantiate(enemiesToSpawn[enemiesToSpawnIndicator], spawnPoint.transform.position, Quaternion.identity);
+            indicateSpawnEnemy();
+            justSpawnedEnemy = Instantiate(enemiesToSpawn[enemiesSpawnIndicator], spawnPoint.transform.position, Quaternion.identity);
             enemiesAlive.Add(justSpawnedEnemy);
             enemiesToSpawn.Remove(justSpawnedEnemy);
         }
@@ -56,9 +63,24 @@ public class SpawnEnemies : MonoBehaviour
 
     public void RoomCleared()
     {
-        print("Cleared!");
-        timerBool = false;
+
+        roomCleared = true;
+    }
+
+    public void indicateSpawnEnemy() //always call this before spawning an enemy
+    {
+        enemiesSpawnIndicator = Random.Range(1, spawnChanceEnemy[0] + spawnChanceEnemy[1] + spawnChanceEnemy[2]);
+        if(enemiesSpawnIndicator <= spawnChanceEnemy[0])
+        {
+            enemiesSpawnIndicator = 0;
+        }
+        else if(enemiesSpawnIndicator <= spawnChanceEnemy[0] + spawnChanceEnemy[1])
+        {
+            enemiesSpawnIndicator = 1;
+        }
+        else if(enemiesSpawnIndicator <= spawnChanceEnemy[0] + spawnChanceEnemy[1] + spawnChanceEnemy[2])
+        {
+            enemiesSpawnIndicator = 2;
+        }
     }
 }
-/*
- */
