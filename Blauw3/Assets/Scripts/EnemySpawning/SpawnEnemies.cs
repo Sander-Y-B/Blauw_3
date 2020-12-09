75e0190283c9b0a205d909eb5102fdb7d01659d5
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-12-09 14:49:02 +0100

    WIP (and nea nea)

--- Blauw3/Assets/Scripts/EnemySpawning/SpawnEnemies.cs
+++ Blauw3/Assets/Scripts/EnemySpawning/SpawnEnemies.cs
@@ -27,26 +27,26 @@ public class SpawnEnemies : MonoBehaviour
 
     void Update()
     {
-        if(roomStarted == true)
+        if (roomStarted == true)
         {
             if (roomCleared == false)
             {
-                if(timeSinceLastSpawn >= minTimeBetweenSpawns && currentEnemies < totalEnemies && enemiesAlive.Count < maxEnemiesAllowed)//spawns more enemies, but not over a certain amount and there is a minimum wait time
+                if (timeSinceLastSpawn >= minTimeBetweenSpawns && currentEnemies < totalEnemies && enemiesAlive.Count < maxEnemiesAllowed)//spawns more enemies, but not over a certain amount and there is a minimum wait time
                 {
                     indicateSpawnEnemy();
-                    justSpawnedEnemy = Instantiate(enemiesToSpawn[enemiesSpawnIndicator], spawnPoints[Random.Range(0 ,spawnPoints.Length)].transform.position, Quaternion.identity);
+                    justSpawnedEnemy = Instantiate(enemiesToSpawn[enemiesSpawnIndicator], spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position, Quaternion.identity);
                     enemiesAlive.Add(justSpawnedEnemy);
-                    justSpawnedEnemy.GetComponent<Enemy>().spawnEnemies = this;
+                    justSpawnedEnemy.GetComponent<TestEnemy>().spawnEnemies = this;
                     currentEnemies++;
                     timeSinceLastSpawn = 0;
                 }
 
-                if(timerBool == true)
+                if (timerBool == true)
                 {
                     timeSinceLastSpawn += Time.deltaTime;
                 }
 
-                if(totalEnemies == currentEnemies && enemiesAlive.Count == 0)
+                if (totalEnemies == currentEnemies && enemiesAlive.Count == 0)
                 {
                     RoomCleared();
                 }
@@ -56,19 +56,21 @@ public class SpawnEnemies : MonoBehaviour
 
     public void RoomStart() //called when player enters the room for the first time (though an on trigger on the player)
     {
-        if(roomCleared == false)
+        if (roomCleared == false)
         {
             //temp close doors (no animation)
-            foreach(GameObject door in doors)
+            foreach (GameObject door in doors)
             {
                 door.gameObject.SetActive(true);
             }
 
-            foreach(GameObject spawnPoint in spawnPoints)
+            foreach (GameObject spawnPoint in spawnPoints)
             {
                 indicateSpawnEnemy();
                 justSpawnedEnemy = Instantiate(enemiesToSpawn[enemiesSpawnIndicator], spawnPoint.transform.position, Quaternion.identity);
                 enemiesAlive.Add(justSpawnedEnemy);
+                justSpawnedEnemy.GetComponent<TestEnemy>().spawnEnemies = this;
+                currentEnemies++;
             }
             timerBool = true;
         }
@@ -79,7 +81,7 @@ public class SpawnEnemies : MonoBehaviour
         //temp open doors(no animation)
         foreach (GameObject door in doors)
         {
-            if (door.activeInHierarchy == false) ;
+            door.SetActive(false);
         }
 
         roomCleared = true;
@@ -89,15 +91,15 @@ public class SpawnEnemies : MonoBehaviour
     {
         enemiesSpawnIndicator = Random.Range(0, spawnChanceEnemy[0] + spawnChanceEnemy[1] + spawnChanceEnemy[2]);
         enemiesSpawnIndicator++;
-        if(enemiesSpawnIndicator <= spawnChanceEnemy[0])
+        if (enemiesSpawnIndicator <= spawnChanceEnemy[0])
         {
             enemiesSpawnIndicator = 0;
         }
-        else if(enemiesSpawnIndicator <= spawnChanceEnemy[0] + spawnChanceEnemy[1])
+        else if (enemiesSpawnIndicator <= spawnChanceEnemy[0] + spawnChanceEnemy[1])
         {
             enemiesSpawnIndicator = 1;
         }
-        else if(enemiesSpawnIndicator <= spawnChanceEnemy[0] + spawnChanceEnemy[1] + spawnChanceEnemy[2])
+        else if (enemiesSpawnIndicator <= spawnChanceEnemy[0] + spawnChanceEnemy[1] + spawnChanceEnemy[2])
         {
             enemiesSpawnIndicator = 2;
         }
---
+++ Blauw3/Assets/Scripts/EnemySpawning/TestEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemy : MonoBehaviour
+{
+    public SpawnEnemies spawnEnemies;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            spawnEnemies.enemiesAlive.Remove(gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
