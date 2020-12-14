18bbc365ab30941c0d9249f7f861eb1bea73943f
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-12-14 16:29:46 +0100

    WIP

---
+++ Blauw3/Assets/Scripts/UI/SecurityLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityLevel : MonoBehaviour
+{
+    public int securityLevel;
+
+    //enemy buffs
+    public float attackSpeed, movementSpeed;
+    public float amount;
+    public float health, damage;
+    public float shield;
+
+    //player nurfs
+    public float healingDown, maxHealthDown;
+    public float ammoRegenDown, maxAmmoDown;
+
+    //other
+    public float timer;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void AddOrSubtractSecurityLevel(int addOrSubtractSecurityLevel)
+    {
+        securityLevel += addOrSubtractSecurityLevel;
+    }
+}
