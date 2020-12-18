19f73ce4d55971fffc13d21fc670e93639edb127
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-12-18 17:02:38 +0100

    WIP

--- Blauw3/Assets/Scripts/UI/SecurityLevel.cs
+++ Blauw3/Assets/Scripts/UI/SecurityLevel.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class SecurityLevel : MonoBehaviour
 {
-    public int securityLevel;
+    static int securityLevel;
 
     //enemy buffs
-    public float attackSpeed, movementSpeed;
-    public float amount;
-    public float health, damage;
-    public float shield;
+    static float attackSpeed;
+    static float movementSpeed;
+    static float amount;
+    static float health;
+    static float damage;
+    static float shield;
 
     //player nurfs
-    public float healingDown, maxHealthDown;
-    public float ammoRegenDown, maxAmmoDown;
+    static float healingDown;
+    static float maxHealthDown;
+    static float ammoRegenDown;
+    static float maxAmmoDown;
 
     //other
-    public float timer;
+    static float timer;
+
+    //individual security levels
+    static float attackSpeedSL;
+    static float movementSpeedSL;
+    static float amountSL;
+    static float healthSL;
+    static float damageSL;
+    static float shieldSL;
+
+    static float healingDownSL;
+    static float maxHealthDownSL;
+    static float ammoRegenDownSL;
+    static float maxAmmoDownSL;
+
+    static float timerSL;
 
     void Start()
     {
