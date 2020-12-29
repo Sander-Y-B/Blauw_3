a7cbc159de38191f387bb44042737b72d6447b35
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-12-29 01:33:51 +0100

    WIP

--- Blauw3/Assets/Scripts/UI/SecurityLevel.cs
+++ Blauw3/Assets/Scripts/UI/SecurityLevel.cs
@@ -7,36 +7,36 @@ public class SecurityLevel : MonoBehaviour
     static int securityLevel;
 
     //enemy buffs
-    static float attackSpeed;
-    static float movementSpeed;
+    static float enemyAttackSpeed;
+    static float enemyMovementSpeed;
     static float enemyAmount;
     static float enemyHealth;
     static float enemyDamage;
     static float enemyShield;
 
     //player nurfs
-    static float healingDown;
-    static float maxHealthDown;
-    static float ammoRegenDown;
-    static float maxAmmoDown;
+    static float playerHealingDown;
+    static float playerMaxHealthDown;
+    static float playerAmmoRegenDown;
+    static float playerMaxAmmoDown;
 
     //other
-    static float timer;
+    static float playerTimer;
 
     //individual security levels
-    static float attackSpeedSL;
-    static float movementSpeedSL;
+    static float enemyAttackSpeedSL;
+    static float enemyMovementSpeedSL;
     static float enemyAmountSL;
     static float enemyHealthSL;
     static float enemyDamageSL;
     static float enemyShieldSL;
 
-    static float healingDownSL;
-    static float maxHealthDownSL;
-    static float ammoRegenDownSL;
-    static float maxAmmoDownSL;
+    static float playerHealingDownSL;
+    static float playerMaxHealthDownSL;
+    static float playerAmmoRegenDownSL;
+    static float playerMaxAmmoDownSL;
 
-    static float timerSL;
+    static float playerTimerSL;
 
     void Start()
     {
@@ -49,33 +49,75 @@ public class SecurityLevel : MonoBehaviour
         
     }
 
-    public void UpdateSecurityLevel()
+    public void UpdateSecurityLevel(int updateSecurityLevel)
     {
-
+        securityLevel += updateSecurityLevel;
+        //update text
     }
 
-    public void AttackSpeedSecurityLevel(int attackSpeedSecurityLevel)
+    public void EnemyAttackSpeedSecurityLevel(int enemyAttackSpeedSecurityLevel)
     {
-        attackSpeedSL += attackSpeedSecurityLevel;
+        enemyAttackSpeedSL += enemyAttackSpeedSecurityLevel;
+        UpdateSecurityLevel(enemyAttackSpeedSecurityLevel);
     }
 
-    public void MovementSpeedSecurityLevel(int movementSpeedSecurityLevel)
+    public void EnemyMovementSpeedSecurityLevel(int enemyMovementSpeedSecurityLevel)
     {
-        movementSpeedSL += movementSpeedSecurityLevel;
+        enemyMovementSpeedSL += enemyMovementSpeedSecurityLevel;
+        UpdateSecurityLevel(enemyMovementSpeedSecurityLevel);
     }
 
     public void EnemyAmountSecurityLevel(int enemyAmountSecurityLevel)
     {
         enemyAmountSL += enemyAmountSecurityLevel;
+        UpdateSecurityLevel(enemyAmountSecurityLevel);
     }
 
     public void EnemyHealthSecurityLevel(int enemyHealthSecurityLevel)
     {
         enemyHealthSL += enemyHealthSecurityLevel;
+        UpdateSecurityLevel(enemyHealthSecurityLevel);
     }
 
     public void EnemyDamageSecurityLevel(int enemyDamageSecurityLevel)
     {
         enemyDamageSL += enemyDamageSecurityLevel;
+        UpdateSecurityLevel(enemyDamageSecurityLevel);
+    }
+
+    public void EnemyShieldSecurityLevel(int enemyShieldSecurityLevel)
+    {
+        enemyShieldSL += enemyShieldSecurityLevel;
+        UpdateSecurityLevel(enemyShieldSecurityLevel);
+    }
+
+    public void PlayerHealingDownSecurityLevel(int playerHealingDownSecurityLevel)
+    {
+        playerHealingDownSL += playerHealingDownSecurityLevel;
+        UpdateSecurityLevel(playerHealingDownSecurityLevel);
+    }
+
+    public void PlayerMaxHealthDownSecurityLevel(int playerMaxHealthDownSecurityLevel)
+    {
+        playerMaxHealthDownSL += playerMaxHealthDownSecurityLevel;
+        UpdateSecurityLevel(playerMaxHealthDownSecurityLevel);
+    }
+
+    public void PlayerAmmoRegenDownSecurityLevel(int playerAmmoRegenDownSecurityLevel)
+    {
+        playerAmmoRegenDownSL += playerAmmoRegenDownSecurityLevel;
+        UpdateSecurityLevel(playerAmmoRegenDownSecurityLevel);
+    }
+
+    public void PlayerMaxAmmoDownSecurityLevel(int playerMaxAmmoDownSecurityLevel)
+    {
+        playerMaxAmmoDownSL += playerMaxAmmoDownSecurityLevel;
+        UpdateSecurityLevel(playerMaxAmmoDownSecurityLevel);
+    }
+
+    public void PlayerTimerSecurityLevel(int playerTimerSecurityLevel)
+    {
+        playerTimerSL += playerTimerSecurityLevel;
+        UpdateSecurityLevel(playerTimerSecurityLevel);
     }
 }
