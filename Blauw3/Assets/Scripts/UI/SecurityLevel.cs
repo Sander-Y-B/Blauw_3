f8c4fcede567616873d8c0c0d548062c2feb43d2
JonasLK <49680607+JonasLK@users.noreply.github.com>
2020-12-31 18:33:45 +0100

    WIP

--- Blauw3/Assets/Scripts/UI/SecurityLevel.cs
+++ Blauw3/Assets/Scripts/UI/SecurityLevel.cs
@@ -38,6 +38,22 @@ public class SecurityLevel : MonoBehaviour
 
     static float playerTimerSL;
 
+    //max securitLevel
+    private float enemyAttackSpeedML;
+    private float enemyMovementSpeedML;
+    private float enemyAmountML;
+    private float enemyHealthML;
+    private float enemyDamageML;
+    private float enemyShieldML;
+
+    private float playerHealingDownML;
+    private float playerMaxHealthDownML;
+    private float playerAmmoRegenDownML;
+    private float playerMaxAmmoDownML;
+
+    private float playerTimerML;
+
+
     void Start()
     {
         
@@ -52,71 +68,115 @@ public class SecurityLevel : MonoBehaviour
     public void UpdateSecurityLevel(int updateSecurityLevel)
     {
         securityLevel += updateSecurityLevel;
-        //update text
+        //update text.text
     }
 
     public void EnemyAttackSpeedSecurityLevel(int enemyAttackSpeedSecurityLevel)
     {
+        if(enemyAttackSpeedSL == 0 && enemyAttackSpeedSecurityLevel < 0 || enemyAttackSpeedSL == enemyAttackSpeedML && enemyAttackSpeedSecurityLevel > 0)
+        {
+            return;
+        }
         enemyAttackSpeedSL += enemyAttackSpeedSecurityLevel;
         UpdateSecurityLevel(enemyAttackSpeedSecurityLevel);
     }
 
     public void EnemyMovementSpeedSecurityLevel(int enemyMovementSpeedSecurityLevel)
     {
+        if (enemyMovementSpeedSL == 0 && enemyMovementSpeedSecurityLevel < 0 || enemyMovementSpeedSL == enemyMovementSpeedML && enemyMovementSpeedSecurityLevel > 0)
+        {
+            return;
+        }
         enemyMovementSpeedSL += enemyMovementSpeedSecurityLevel;
         UpdateSecurityLevel(enemyMovementSpeedSecurityLevel);
     }
 
     public void EnemyAmountSecurityLevel(int enemyAmountSecurityLevel)
     {
+        if (enemyAmountSL == 0 && enemyAmountSecurityLevel < 0 || enemyAmountSL == enemyAmountML && enemyAmountSecurityLevel > 0)
+        {
+            return;
+        }
         enemyAmountSL += enemyAmountSecurityLevel;
         UpdateSecurityLevel(enemyAmountSecurityLevel);
     }
 
     public void EnemyHealthSecurityLevel(int enemyHealthSecurityLevel)
     {
+        if (enemyHealthSL == 0 && enemyHealthSecurityLevel < 0 || enemyHealthSL == enemyHealthML && enemyHealthSecurityLevel > 0)
+        {
+            return;
+        }
         enemyHealthSL += enemyHealthSecurityLevel;
         UpdateSecurityLevel(enemyHealthSecurityLevel);
     }
 
     public void EnemyDamageSecurityLevel(int enemyDamageSecurityLevel)
     {
+        if (enemyDamageSL == 0 && enemyDamageSecurityLevel < 0 || enemyDamageSL == enemyDamageML && enemyDamageSecurityLevel > 0)
+        {
+            return;
+        }
         enemyDamageSL += enemyDamageSecurityLevel;
         UpdateSecurityLevel(enemyDamageSecurityLevel);
     }
 
     public void EnemyShieldSecurityLevel(int enemyShieldSecurityLevel)
     {
+        if (enemyShieldSL == 0 && enemyShieldSecurityLevel < 0 || enemyShieldSL == enemyShieldML && enemyShieldSecurityLevel > 0)
+        {
+            return;
+        }
         enemyShieldSL += enemyShieldSecurityLevel;
         UpdateSecurityLevel(enemyShieldSecurityLevel);
     }
 
     public void PlayerHealingDownSecurityLevel(int playerHealingDownSecurityLevel)
     {
+        if (playerHealingDownSL == 0 && playerHealingDownSecurityLevel < 0 || playerHealingDownSL == playerHealingDownML && playerHealingDownSecurityLevel > 0)
+        {
+            return;
+        }
         playerHealingDownSL += playerHealingDownSecurityLevel;
         UpdateSecurityLevel(playerHealingDownSecurityLevel);
     }
 
     public void PlayerMaxHealthDownSecurityLevel(int playerMaxHealthDownSecurityLevel)
     {
+        if (playerMaxHealthDownSL == 0 && playerMaxHealthDownSecurityLevel < 0 || playerMaxHealthDownSL == playerMaxHealthDownML && playerMaxHealthDownSecurityLevel > 0)
+        {
+            return;
+        }
         playerMaxHealthDownSL += playerMaxHealthDownSecurityLevel;
         UpdateSecurityLevel(playerMaxHealthDownSecurityLevel);
     }
 
     public void PlayerAmmoRegenDownSecurityLevel(int playerAmmoRegenDownSecurityLevel)
     {
+        if (playerAmmoRegenDownSL == 0 && playerAmmoRegenDownSecurityLevel < 0 || playerAmmoRegenDownSL == playerAmmoRegenDownML && playerAmmoRegenDownSecurityLevel > 0)
+        {
+            return;
+        }
         playerAmmoRegenDownSL += playerAmmoRegenDownSecurityLevel;
         UpdateSecurityLevel(playerAmmoRegenDownSecurityLevel);
     }
 
     public void PlayerMaxAmmoDownSecurityLevel(int playerMaxAmmoDownSecurityLevel)
     {
+        if (playerMaxAmmoDownSL == 0 && playerMaxAmmoDownSecurityLevel < 0 || playerMaxAmmoDownSL == playerMaxAmmoDownML && playerMaxAmmoDownSecurityLevel > 0)
+        {
+            return;
+        }
         playerMaxAmmoDownSL += playerMaxAmmoDownSecurityLevel;
         UpdateSecurityLevel(playerMaxAmmoDownSecurityLevel);
     }
 
     public void PlayerTimerSecurityLevel(int playerTimerSecurityLevel)
     {
+        if (playerTimerSL == 0 && playerTimerSecurityLevel < 0 || playerTimerSL == playerTimerML && playerTimerSecurityLevel > 0)
+        {
+            return;
+        }
         playerTimerSL += playerTimerSecurityLevel;
         UpdateSecurityLevel(playerTimerSecurityLevel);
     }
