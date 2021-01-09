c0cfdd0781d614c055bbad49f05d278bbc6cff69
EdgarSabel <60340099+EdgarSabel@users.noreply.github.com>
2021-01-09 19:37:03 +0100

    new room decorated + Dummy

---
+++ Blauw3/Assets/Scripts/Enemies/Dummy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dummy : MonoBehaviour
+{
+    public GameObject prefab;
+    public float health, dissolveTime;
+    private float maxHealth, healthPercent;
+    public Slider healthSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0) { DestroyEnemy(); }
+        healthPercent = health / maxHealth * 100;
+        healthSlider.value = healthPercent;
+    }
+
+    public virtual void DestroyEnemy()
+    {
+        health = maxHealth;
+    }
+
+}
