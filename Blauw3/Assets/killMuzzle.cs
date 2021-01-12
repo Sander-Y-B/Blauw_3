1d92be70120590b913c30a4d4f1bbfe1a76bdb32
EdgarSabel <60340099+EdgarSabel@users.noreply.github.com>
2021-01-12 17:02:04 +0100

    audio level update + muzzle despawn

---
+++ Blauw3/Assets/killMuzzle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killMuzzle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Pog());   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Pog()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+}
